37721aea74dc41bd82f8fbe3b98333c78162b533
teachercheckin <141989113+teachercheckin@users.noreply.github.com>
2023-08-26 13:54:25 -0700

    Delete src directory

--- src/api/spreadsheetID.js
+++
@@ -1,2 +0,0 @@
-export const spreadsheetID = {production: "1FqeRNFlZZ7aJdtYRmjZ1kRb2-sarN9aoTMGyu4jhGKs",
-                              development: "1FqeRNFlZZ7aJdtYRmjZ1kRb2-sarN9aoTMGyu4jhGKs"};
